3d3e7a14417196746fb4c8f74a6fada1df0cea14
kacper.zielinski <kacper.zielinski09@gmail.com>
2019-03-10 21:31:30 +0100

    Java 8 Optional primary tests added

---
+++ src/com/java/demos/java8/OptionalDemo.java
@@ -0,0 +1,87 @@
+package com.java.demos.java8;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
+class OptionalDemo {
+
+    @Test
+    void innerAnonymousClassVersusLambdaExpression() {
+        Runnable r1 = new Runnable() {
+
+            @Override public void run() {
+                System.out.println("Runner inner whoohaaa.");
+            }
+        };
+        r1.run();
+
+        Runnable r2 = () -> System.out.println("Runned by lambda");
+        r2.run();
+    }
+
+    @Test
+    void shouldThrowNullPointerExceptionWhenOfUsed() {
+        assertThrows(NullPointerException.class, () -> Optional.of(null));
+    }
+
+    @Test
+    void shouldNotThrowExceptionWhenOfNullableUsed() {
+        assertDoesNotThrow(() -> Optional.ofNullable(null));
+    }
+
+    @Test
+    void shouldThrowNoSuchElementExceptionWhenNullValueInOfNullableOptionalUsed() {
+        assertThrows(NoSuchElementException.class, () -> {
+            final Optional<String> nullableString = Optional.ofNullable(null);
+            nullableString.get();
+        });
+    }
+
+    @Test
+    void shouldThrowNoSuchElementExceptionWhenGetWithEmptyOptionalInvoked() {
+        assertThrows(NoSuchElementException.class, () -> {
+            final Optional<String> emptyOptional = Optional.empty();
+            emptyOptional.get();
+        });
+    }
+
+    @Test
+    void shouldNotThrowExceptionWhenIfPresentUsed() {
+        final Optional<String> nullableString = Optional.ofNullable(null);
+        nullableString.ifPresent(s -> fail());
+    }
+
+    @Test
+    void shouldReturnProperValueFromOptional() {
+        final String name = "Kacper";
+        Optional<String> optionalName = Optional.ofNullable(name);
+        assertEquals(name, optionalName.get());
+    }
+
+    @Test
+    void shouldReturnDefaultWhenValueIsEmpty() {
+        final String nullableString = null;
+        final String notEmptyString = "Kacper";
+
+        String value = Optional.ofNullable(nullableString).orElse(notEmptyString);
+        assertEquals(value, notEmptyString);
+    }
+
+    @Test
+    void shouldIgnoreDefaultWhenValueIsNotEmpty() {
+        final String nonEmptyString1 = "Java";
+        final String nonEmptyString2 = "Kacper";
+
+        String value = Optional.ofNullable(nonEmptyString1).orElse(nonEmptyString2);
+        assertEquals(value, nonEmptyString1);
+    }
+    
+
+}
