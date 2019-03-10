2e5783c7e18685e197678c1f2b5da0f23c2674dc
= <236698@student,pwr.edu.pl>
2019-03-10 22:00:00 +0100

    Added simple Stream with collector test

--- src/com/java/demos/java8/StreamDemo.java
+++ src/com/java/demos/java8/StreamDemo.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +21,22 @@ class StreamDemo {
                 .count();
 
         assertEquals(counter, 2L);
+    }
+
+    @Test
+    void shouldConcatenateListsOneByOne()
+    {
+        List<String> myList1 =
+                Arrays.asList("a11", "a21", "b11", "cb21", "ca11");
+
+        List<String> myList2 =
+                Arrays.asList("2a12", "a22", "b12", "cb22", "ca12");
+
+        List<String> concatenatedList = Stream.concat(myList1.stream(), myList2.stream())
+                .collect(Collectors.toList());
+
+        assertEquals(concatenatedList.get(0), myList1.get(0));
+        assertEquals(concatenatedList.get(5), myList2.get(0));
+        assertEquals(concatenatedList.get(9), myList2.get(4));
     }
 }
